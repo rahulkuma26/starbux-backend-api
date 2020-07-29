eb8b0ed3d43c7af5b023c40f0a08bb71547fb999
Rahul Kumar <rahul.kumar@ingka.com>
2020-07-29 15:32:55 +0200

    Intial commit

---
+++ src/main/java/com/starbux/ecommerce/api/StarbuxEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.starbux.ecommerce.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarbuxEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarbuxEcommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/starbux/ecommerce/api/StarbuxEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.starbux.ecommerce.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarbuxEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
