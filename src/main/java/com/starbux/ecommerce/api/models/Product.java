587b6bd1e65a84455f5c01f498a5f91d398d4b77
Rahul Kumar <rahul.kumar@ingka.com>
2020-07-31 12:37:29 +0200

    Intial commit, Added: 1. Intial Test cases 2. Entityies 3. Repository 4. Services 5. controllers 6. Utill

--- src/main/java/com/starbux/ecommerce/api/models/Product.java
+++ src/main/java/com/starbux/ecommerce/api/models/Product.java
@@ -4,6 +4,7 @@ import lombok.Data;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 
 /**
  * This class will represent our product entity and its attributes
@@ -18,20 +19,23 @@ public class Product {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long product_Id;  // Each product will be given an auto-generated unique identifier when stored
 
-    @Column(name = "product_name", nullable = false)
+    @NotNull(message = "Product name is required.")
+    @Column(name = "product_name")
     private String productName; //  Name of the product
 
-    @Column(name = "product_amount", nullable = false)
-    private int productAmount; // Amount of the product
+    @NotNull(message = "Product amount is required.")
+    @Column(name = "product_amount")
+    private double productAmount; // Amount of the product
 
-    @Column(name = "product_type", nullable = false)
+    @NotNull(message = "Product type is required.")
+    @Column(name = "product_type")
     private String productType; // Type of the product - it can be Drink or Topping as of know
 
     public Product() {
         super();
     }
 
-    public Product(String productName, int productAmount, String productType) {
+    public Product(@NotNull(message = "Product name is required.") String productName, @NotNull(message = "Product amount is required.") double productAmount, @NotNull(message = "Product type is required.") String productType) {
         this.productName = productName;
         this.productAmount = productAmount;
         this.productType = productType;
